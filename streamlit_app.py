1fd9794f69d5017e8515e637896822e03b1c4623
Yin Sun <sunyin51@gmail.com>
2022-07-02 12:39:38 -0700

    demo bidding from all players

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,21 @@
+from cv2 import bitwise_and
 from sklearn.metrics import auc
 import streamlit as st
 
-import asyncio
 import sys
 sys.path.append('src')
 
 from game import Models, conf
 from bots import BotBid
 
-st.text('start')
+
+def st_print(hand):
+    lines = []
+    suits = hand.split('.')
+    for i in range(4):
+        s = 'SHDC'[i]
+        lines.append(f'{s} {suits[i]}')
+    return lines
 
 models = Models.from_conf(conf.load('default.conf'))
 
@@ -16,9 +23,9 @@ models = Models.from_conf(conf.load('default.conf'))
 vuln_ns, vuln_ew = False, True
 
 # you sit West and hold:
-hand = '73.KJ83.AT2.T962'
+hands = 'A8.Q752.T54.JT63 K54.T643.A92.A72 JT932.K9.Q86.854 Q76.AJ8.KJ73.KQ9'
 
-input_text = st.text_input('card bids play', f'{hand} _ 1D 1S')
+input_text = st.text_input('card bids play', f'{hands} _ p')
 
 # the auction goes:
 # (a few words about 'PAD_START':
@@ -28,9 +35,10 @@ input_text = st.text_input('card bids play', f'{hand} _ 1D 1S')
 # if South deals we have two pads (because North and East are skipped)
 # etc.)
 inputs = input_text.split()
-hand = inputs[0]
+hands = inputs[:4]
+bidder_bots = [BotBid([vuln_ns, vuln_ew], hand, models) for hand in hands]
 auction = []
-for bid in inputs[1:]:
+for bid in inputs[4:]:
     if bid == '_':
         bid = 'PAD_START'
     if bid.lower() == 'p':
@@ -39,13 +47,52 @@ for bid in inputs[1:]:
         bid = bid.upper()
     auction.append(bid)
 
-bot_bid = BotBid([vuln_ns, vuln_ew], hand, models)
+turn = len(auction) % 4
+lines = [f'Vul: NS {vuln_ns} EW {vuln_ew}']
+layout = [(0, 1), (1, 2), (2, 1), (1, 0)]
+row = [[[]]*3, [[]]*3, [[]]*3]
 
-st.text(bot_bid.bid(auction).bid)
+for i in range(4):
+    r,col = layout[i]
+    suits = st_print(hands[i])
+    row[r][col] = suits
 
+# nice ascii graph to display cards and bidding
+for r in range(3):
+    for subr in range(4):
+        line = ''
+        for col in range(3):
+            if len(row[r][col]) == 0:
+                if col == 0:
+                    line += ' '*7
+                elif col == 1:
+                    line += ' '*(15-len(line))
+            else:
+                line += row[r][col][subr]
+        lines.append(line)    
+line = 'North East  South West'
+lines.append(line)
+line = ''
+for i in range(len(auction)):
+    bid = auction[i]
+    if bid == 'PAD_START':
+        bid = ' '*6
+    if len(bid) < 6:
+        bid += ' '*(6-len(bid))
+    if line != '' and i % 4 == 0:
+        lines.append(line)
+        line = ''
+    line += bid
+i = len(auction)
+if line != '' and i % 4 == 0:
+    lines.append(line)
+    line = ''
+bid = bidder_bots[turn].bid(auction).bid
+line += bid
+lines.append(line)
 
+st.text('\n'.join(lines))
 
-st.text('end')
 
 
 
