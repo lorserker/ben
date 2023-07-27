cf049882731f2e1084dbd9352757c4790d4a7d7e
Thorvald Aagaard <thorvald@netbridge.dk>
2023-07-27 16:27:32 +0200

    Fixed error in multimatches

--- scripts/match/bidding/vs.cmd
+++ scripts/match/bidding/vs.cmd
@@ -1,8 +1,14 @@
 set a=%1
 set b=%2
+set boards=set0010.txt
 
-python compare.py .\%a%\results1.json .\%b%\results2.json > .\%a%vs%b%\compare.json 
+copy .\%a%\results1.json .\%a%vs%b%\results1.json
+copy .\%a%\results2.json .\%a%vs%b%\results2.json
+
+python compare.py .\%a%vs%b%\results1.json .\%a%vs%b%\results2.json > .\%a%vs%b%\compare.json 
 
 type ".\%a%vs%b%\compare.json" | python printmatch.py >.\%a%vs%b%\match.txt
 
-type ".\%a%vs%b%\compare.json" | python printmatchashtml.py >.\%a%vs%b%\html\match.txt
+type ".\%a%vs%b%\compare.json" | python printmatchashtml.py >.\%a%vs%b%\html\match.html
+
+python printdeal.py %boards% %a%vs%b%
