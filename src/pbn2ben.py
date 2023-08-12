525e09252d1ff6b0f8a84cda97f40db841707467
Thorvald Aagaard <thorvald@netbridge.dk>
2023-08-12 20:21:49 +0200

    Conversion between PBN and BEN-format

--- src/pbn2ben.py
+++ src/pbn2ben.py
@@ -7,25 +7,27 @@ class Deal(NamedTuple):
     vulnerable: str
     hands: str
 
+
 def load(fin):
+    boards = [] 
     dealer, vulnerable = None, None
-    with open('input.ben', 'w', encoding='utf-8') as file:  # Open the output file with UTF-8 encoding
-        for line in fin:
-            if line.startswith('[Dealer'):
-                dealer = extract_value(line)
-            elif line.startswith('[Vulnerable'):
-                vuln_str = extract_value(line)
-                vulnerable = {'NS': 'N-S', 'EW': 'E-W', 'All': 'Both'}.get(vuln_str, vuln_str)
-            elif line.startswith('[Deal'):
-                hands_pbn = extract_value(line)
-                [seat, hands] = hands_pbn.split(':')
-                hands_nesw = [''] * 4
-                first_i = 'NESW'.index(seat)
-                for hand_i, hand in enumerate(hands.split()):
-                    hands_nesw[(first_i + hand_i) % 4] = hand
-                print("'" + ' '.join(hands_nesw) + "','" + dealer + " " + vulnerable + "' ", file=file)  # Print to the file
-            else:
-                continue
+    for line in fin:
+        if line.startswith('[Dealer'):
+            dealer = extract_value(line)
+        elif line.startswith('[Vulnerable'):
+            vuln_str = extract_value(line)
+            vulnerable = {'NS': 'N-S', 'EW': 'E-W', 'All': 'Both'}.get(vuln_str, vuln_str)
+        elif line.startswith('[Deal'):
+            hands_pbn = extract_value(line)
+            [seat, hands] = hands_pbn.split(':')
+            hands_nesw = [''] * 4
+            first_i = 'NESW'.index(seat)
+            for hand_i, hand in enumerate(hands.split()):
+                hands_nesw[(first_i + hand_i) % 4] = hand
+            boards.append("'" + ' '.join(hands_nesw) + "','" + dealer + " " + vulnerable + "'") 
+        else:
+            continue
+    return boards
 
 def extract_value(s: str) -> str:
     return s[s.index('"') + 1 : s.rindex('"')]
@@ -39,7 +41,14 @@ if __name__ == '__main__':
     try:
         with open(input_file, "r", encoding='utf-8') as file:  # Open the input file with UTF-8 encoding
             lines = file.readlines()
-        load(lines)
+        
+        with open('input.ben', 'w', encoding='utf-8') as file:  # Open the output file with UTF-8 encoding
+            boards = load(lines)
+            print(boards)
+            for board in boards:
+                print(board)
+                file.write(board + "\n")
+
     except Exception as ex:
         print('Error:', ex)
         raise ex
