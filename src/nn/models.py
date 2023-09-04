b179fef6ef218e4c4dc91d07ab1bf3bd039b3d82
Thorvald Aagaard <thorvald@netbridge.dk>
2023-09-04 03:04:53 +0200

    Added search_threshold

--- src/nn/models.py
+++ src/nn/models.py
@@ -12,32 +12,38 @@ from nn.lead_singledummy import LeadSingleDummy
 
 class Models:
 
-    def __init__(self, bidder_model, binfo, lead, sd_model, player_models):
+    def __init__(self, bidder_model, binfo, lead, sd_model, player_models, search_threshold):
         self.bidder_model = bidder_model
         self.binfo = binfo
         self.lead = lead
         self.sd_model = sd_model
         self.player_models = player_models
+        self._search_threshold = search_threshold
 
     @classmethod
     def from_conf(cls, conf: ConfigParser, base_path=None) -> "Models":
         if base_path is None:
             base_path = os.getenv('BEN_HOME') or '..'
+        try:
+            search_threshold = float(conf['bidding']['search_threshold'])
+        except KeyError:
+            # Handle the case where 'search_threshold' key is missing
+            search_threshold = 0.10 # default
         return cls(
-            bidder_model=Bidder('bidder', os.path.join(
-                base_path, conf['bidding']['bidder'])),
+            bidder_model=Bidder('bidder', os.path.join(base_path, conf['bidding']['bidder'])),
             binfo=BidInfo(os.path.join(base_path, conf['bidding']['info'])),
             lead=Leader(os.path.join(base_path, conf['lead']['lead'])),
-            sd_model=LeadSingleDummy(os.path.join(
-                base_path, conf['eval']['lead_single_dummy'])),
+            sd_model=LeadSingleDummy(os.path.join(base_path, conf['eval']['lead_single_dummy'])),
             player_models=[
-                player.BatchPlayerLefty('lefty', os.path.join(
-                    base_path, conf['cardplay']['lefty'])),
-                player.BatchPlayer('dummy', os.path.join(
-                    base_path, conf['cardplay']['dummy'])),
-                player.BatchPlayer('righty', os.path.join(
-                    base_path, conf['cardplay']['righty'])),
-                player.BatchPlayer('decl', os.path.join(
-                    base_path, conf['cardplay']['decl']))
+                player.BatchPlayerLefty('lefty', os.path.join(base_path, conf['cardplay']['lefty'])),
+                player.BatchPlayer('dummy', os.path.join(base_path, conf['cardplay']['dummy'])),
+                player.BatchPlayer('righty', os.path.join(base_path, conf['cardplay']['righty'])),
+                player.BatchPlayer('decl', os.path.join(base_path, conf['cardplay']['decl']))
             ],
+            search_threshold=search_threshold
         )
+    
+    @property
+    def search_threshold(self):
+        return self._search_threshold
+    
