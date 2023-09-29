0bff6c5a433f53e6ea0cb63e359f5530d7cc1cab
Thorvald Aagaard <thorvald@netbridge.dk>
2023-09-29 15:16:00 +0200

    Added auction to PBN-file

--- scripts/data/epbot/training2pbn.py
+++ scripts/data/epbot/training2pbn.py
@@ -12,19 +12,36 @@ class Deal(NamedTuple):
 
 def pbn_generator(boards):
 
-    def print_deal(board_number, dlr, vul, deal):
-        print('[Event "?"]')
-        print('[Site "?"]')
+    def print_deal(board_number, dlr, vul, deal, dealer, auction):
+        print('[Event "BEN"]')
+        print('[Site "BEN"]')
         print(f'[Date "{datetime.datetime.now().date().isoformat()}"]')
         print(f'[Board "{board_number}"]')
-        print('[West "?"]')
-        print('[North "?"]')
-        print('[East "?"]')
-        print('[South "?"]')
+        print('[West "BEN"]')
+        print('[North "BEN"]')
+        print('[East "BEN"]')
+        print('[South "BEN"]')
         print('[Scoring "IMP"]')
         print(f'[Dealer "{dlr}"]')
         print(f'[Vulnerable "{vul}"]')
         print(f'[Deal "{deal}"]')
+        if len(auction) > 0: 
+            print(f'[Auction "{dealer}"]')
+            # Define the number of elements per line
+            elements_per_line = 4
+
+            # Iterate through the array
+            for i, element in enumerate(auction):
+            # Print the element
+                print(element, end=" ")
+
+                # Check if it's time to insert a line feed
+                if (i + 1) % elements_per_line == 0:
+                    print()  # Insert a line feed
+
+            # Ensure the last line ends with a line feed
+            if len(auction) % elements_per_line != 0:
+                print()
         print('[Declarer "?"]')
         print('[Result "?"]\n')
 
@@ -33,7 +50,7 @@ def pbn_generator(boards):
         parts = boards[i*2+1].split()
         dealer_str = parts[0]
         vulnerable_str = parts[1].replace("-","")
-        print_deal(i + 1, dealer_str, vulnerable_str, f'N:{deal_str}')
+        print_deal(i + 1, dealer_str, vulnerable_str, f'N:{deal_str}', dealer_str, parts[2:])
         
 
 if __name__ == '__main__':
