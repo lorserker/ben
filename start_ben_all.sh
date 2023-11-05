cee26eac6dcfed0db6b1d9fe54d024b7b89a4b6c
Larry Cai <larry.caiyu@gmail.com>
2023-11-05 17:16:24 +0100

    improve
    Signed-off-by: Larry Cai <larry.caiyu@gmail.com>

--- start_ben_all.sh
+++ start_ben_all.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"/src
+
+# is it a bug?
+if [ ! -d models ]; then
+    ln -s ../models models
+fi
+
 # this is all in one wrapper script mainly for container
-#
 python3 gameserver.py & # listen on 4443 for websocket
-python3 appserver.py &    # listen on 8080 for browser
+python3 appserver.py &  # listen on 8080 for browser
 
 # Wait for any process to exit
 wait -n
