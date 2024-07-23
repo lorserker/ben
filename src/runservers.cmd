30101bcd9341c17490997ecee68eab0873628c6c
Thorvald Aagaard <Thorvald@aalborgdata.dk>
2024-07-23 19:38:32 +0200

    Config update and some extra boards

--- src/runservers.cmd
+++ src/runservers.cmd
@@ -1,11 +1,12 @@
 cd frontend
 start "App Server -Local" python appserver.py --host 0.0.0.0
 cd ..
+timeout /t 2 /nobreak >nul
 start "App Server -Local :8081" python appserver.py --port 8081
 timeout /t 2 /nobreak >nul
-start "TF2 Api :80" python gameapi.py --config config\default_TF2.conf --port 80 --host 0.0.0.0
+start "TF2 Api external :80" python gameapi.py --config config\default_TF2.conf --port 80 --host 0.0.0.0
 timeout /t 5 /nobreak >nul
-start "TF2 Api :8085" python gameapi.py --config config\default_TF2.conf --host 0.0.0.0
+start "TF2 Api internal :8085" python gameapi.py --config config\default_TF2.conf --host 0.0.0.0
 timeout /t 5 /nobreak >nul
 start "Old Api :8088" python gameapi.py --host 0.0.0.0 --config config\default_Api.conf --port 8088
 timeout /t 2 /nobreak >nul
